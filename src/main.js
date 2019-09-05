246411fb7c2d92d6169cd26325424bfa18e31f31
Dominique Youmans, Jason Huels and nikhil batta <dominique+jason+nikhil@>
2019-09-05 12:46:46 -0700

    add vowel check

--- src/main.js
+++ src/main.js
@@ -7,19 +7,32 @@ export function getSyllables(word) {
 
   for(let i=0; i<word.length; i++) {
     if(reg.test(word[i])) {
-      newWord += word[i];
+      newWord += word[i].toLowerCase();
     }
   }
 
   if(newWord.length === 1) {
     count = 1;
   } else {
-    // for(let i=0; i<DIPHTHONGS.length; i++) {
-    //   if(newWord.includes(DIPHTHONGS[i])) {
-    //     count++;
-    //     newWord = newWord.slice(newWord.search(DIPHTHONGS[i]), 2);
-    //   }
-    // }
+    if(newWord[newWord.length-1] === 'e') {
+      if(newWord[newWord.length-2] === 'l' || newWord[newWord.length-2] === 'r') {
+        count++;
+        newWord = newWord.slice(newWord.length-2, newWord.length-1);
+      }
+      newWord = newWord.slice(newWord.length-1, newWord.length);
+    }
+    for(let i=0; i<DIPHTHONGS.length; i++) {
+      if(newWord.includes(DIPHTHONGS[i])) {
+        count++;
+        newWord = newWord.slice(newWord.search(DIPHTHONGS[i]), newWord.search(DIPHTHONGS[i]+2));
+      }
+    }
+    for(let i=0; i<VOWELS.length; i++) {
+      if(newWord.includes(VOWELS[i])) {
+        count++;
+        newWord = newWord.slice(newWord.search(VOWELS[i]), newWord.search(VOWELS[i]+1));
+      }
+    }
 
   }
 
